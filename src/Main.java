9466d13e2a613a0aa0d7b0b634363b2c588f87d2
Likitha Rani Dorai <likitharanidorai98@gmail.com>
2024-08-08 11:52:30 -0500

    First Commit on 8/8/24

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         int n=5;
         for (int i=1 ;i<=n;i++) {
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print("1 ");
             }
             for (int k = 1; k <= i; k++) {
                 System.out.print("* ");
